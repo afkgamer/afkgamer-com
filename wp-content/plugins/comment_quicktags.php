cb8b3e20b7070a97a6f97af9f89f7c32cb6ab5e9
afkgamer <49187282+afkgamer@users.noreply.github.com>
2019-04-02 06:37:56 -0500

    Initial commit

---
+++ wp-content/plugins/comment_quicktags.php
@@ -0,0 +1,55 @@
+
+var edButtons = new Array();
+edButtons[edButtons.length] = 
+new edButton('ed_strong'
+,'b'
+,'<strong>'
+,'</strong>'
+,'b'
+);
+
+
+edButtons[edButtons.length] = 
+new edButton('ed_em'
+,'i'
+,'<em>'
+,'</em>'
+,'i'
+);
+
+
+edButtons[edButtons.length] = 
+new edButton('ed_link'
+,'link'
+,''
+,'</a>'
+,'a'
+); // special case
+
+
+edButtons[edButtons.length] = 
+new edButton('ed_block'
+,'bquote'
+,'<blockquote>'
+,'</blockquote>'
+,'q'
+);
+
+
+edButtons[edButtons.length] = 
+new edButton('ed_pre'
+,'code'
+,'<code>'
+,'</code>'
+,'c'
+);
+
+
+edButtons[edButtons.length] = 
+new edButton('ed_strike'
+,'strike'
+,'<strike>'
+,'</strike>'
+,'s'
+);
+
